83b12c8cb9493fe335c8af02075cf52a23efed0c
Arsalaan Ali <arsalaanali2@gmail.com>
2022-12-23 22:03:57 -0500

    database setup

--- webscrape.py
+++ webscrape.py
@@ -1,11 +1,68 @@
 import requests
 import pickle
 from bs4 import BeautifulSoup
-URL = "https://generated.photos/faces/natural/young-adult/black-race/brown-hair/short/neutral/male/brown-eyes"
+URL = "https://generated.photos/faces/natural/young-adult/black-race/black-hair/short/neutral/male/brown-eyes"
 r = requests.get(URL)
+sex = "male"
+
+attr = {"sex": sex,
+    "race": "black-race",
+    "hair_color": "dark-hair",
+    "hair_length": "short-hair",
+    "eye_color": "brown-eyes",
+    "expression": "joy"}
+
 
 soup = BeautifulSoup(r.text, 'html.parser')
 mydivs = soup.findAll('div', class_="card-image")
+
+'''
+#RESET DATABASE
+
+faceDatabase = {"by_attr": {"male":{}, "female":{}}, "all":{}}
+
+with open('faceDatabase.pkl', 'wb') as f:  # SAVES DATABASE TO DISK
+    pickle.dump(faceDatabase, f)
+'''
+
+
+
+
+'''
+with open('faceDatabase.pkl', 'rb') as f: #LOADS DATABASE FROM DISK
+    faceDatabase = pickle.load(f)
+
+def addToDatabase(image):
+    for key, value in attr.items():
+        if key == "sex":
+            continue
+        if value in faceDatabase["by_attr"][sex]:
+            faceDatabase["by_attr"][sex][value].append(image)
+        else:
+            faceDatabase["by_attr"][sex][value] = [image]
+    faceDatabase["all"][image] = attr
+    return
+
+
+
+
+for idx, div in enumerate(mydivs):
+    if(idx>30):
+        break
+    print(idx)
+    imageLink = div.find("img")["src"]
+    addToDatabase(imageLink)
+
+
+
+
+
+with open('faceDatabase.pkl', 'wb') as f:  # SAVES DATABASE TO DISK
+    pickle.dump(faceDatabase, f)
+
+
+print("done")
+'''
 
 '''
 Database Structure:
@@ -23,50 +80,11 @@ faceDatabase: {}
         img: {all attrs of img}
         img2: {all attrs of img2}
         etc.
+
+usedAttrs:
+black-race
+dark-hair
+long-hair
+brown-eyes
+joy
 '''
-# with open('faceDatabase.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-#     pickle.dump(faceDatabase, f)
-
-# with open('faceDatabase.pkl', 'rb') as f:
-#     faceDatabase = pickle.load(f)
-
-attr = {"sex": "male",
-    "race": "black-race",
-    "hair_color": "dark-hair",
-    "hair_length": "long-hair",
-    "eye_color": "brown-eyes",
-    "smiling": False}
-faceDatabase = {
-    "by_attr": {
-        "male": {
-            "white-race": {},
-            "black-race": {},
-            "latino-race": {},
-            "asian-race": {},
-            "brown-eyes": {},
-            "grey-eyes": {},
-            "blue-eyes": {},
-            "green-eyes": {},
-            "brown-hair": {},
-            "black-hair": {},
-            "blond-hair": {},
-            "short-hair": {},
-            "medium-hair": {},
-            "long-hair": {},
-            "neutral-emotion": {},
-            "joy-emotion": {},
-        }
-    }
-}
-
-
-# with open('faceDatabase.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
-    # pickle.dump(faceDatabase, f)
-
-
-
-
-
-
-# for div in mydivs:
-    # print(div.find("img")["src"])
