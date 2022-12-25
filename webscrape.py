ed71ac55c55e1f432aa970b0dd6a81b9715680b3
Arsalaan Ali <arsalaanali2@gmail.com>
2022-12-24 20:24:30 -0500

    listing all attributes

--- webscrape.py
+++ webscrape.py
@@ -1,68 +1,57 @@
 import requests
 import pickle
 from bs4 import BeautifulSoup
-URL = "https://generated.photos/faces/natural/young-adult/black-race/black-hair/short/neutral/male/brown-eyes"
-r = requests.get(URL)
-sex = "male"
+URL = "https://generated.photos/faces/natural/young-adult/white-race/black-hair/short/neutral/male/brown-eyes"
 
-attr = {"sex": sex,
+#SET YOURSELF
+#BLOND FEMALE LONG HAIR (SHORT HAIRS LOOK TOO YOUNG) 
+#
+
+
+attr = {"sex": "male",
     "race": "black-race",
     "hair_color": "dark-hair",
     "hair_length": "short-hair",
     "eye_color": "brown-eyes",
-    "expression": "joy"}
+    "expression": "neutral"}
 
+eyes = ["blue-eyes", "green-eyes", "brown-eyes", "grey-eyes"]
+races = ["black-race", "white-race", "latino-race", "asian-race"]
+hairs = ["black-hair", "brown-hair", "blond-hair"]
+hairLength = ["long", "medium", "short"]
 
-soup = BeautifulSoup(r.text, 'html.parser')
-mydivs = soup.findAll('div', class_="card-image")
-
-'''
-#RESET DATABASE
-
-faceDatabase = {"by_attr": {"male":{}, "female":{}}, "all":{}}
-
-with open('faceDatabase.pkl', 'wb') as f:  # SAVES DATABASE TO DISK
-    pickle.dump(faceDatabase, f)
-'''
-
-
-
-
-'''
 with open('faceDatabase.pkl', 'rb') as f: #LOADS DATABASE FROM DISK
     faceDatabase = pickle.load(f)
 
+def addFacesFromUrl(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    mydivs = soup.findAll('div', class_="card-image")
+
+    for idx, div in enumerate(mydivs):
+        if(idx>30):
+            break
+        print(idx)
+        imageLink = div.find("img")["src"]
+        addToDatabase(imageLink)
+    return
+
 def addToDatabase(image):
+    if(image in faceDatabase["all"]):
+        return
     for key, value in attr.items():
         if key == "sex":
             continue
-        if value in faceDatabase["by_attr"][sex]:
-            faceDatabase["by_attr"][sex][value].append(image)
-        else:
-            faceDatabase["by_attr"][sex][value] = [image]
+        if not (value in faceDatabase["by_attr"][sex]):
+            faceDatabase["by_attr"][sex][value] = {}
+        faceDatabase["by_attr"][sex][value][image] = True
     faceDatabase["all"][image] = attr
     return
-
-
-
-
-for idx, div in enumerate(mydivs):
-    if(idx>30):
-        break
-    print(idx)
-    imageLink = div.find("img")["src"]
-    addToDatabase(imageLink)
-
-
-
-
 
 with open('faceDatabase.pkl', 'wb') as f:  # SAVES DATABASE TO DISK
     pickle.dump(faceDatabase, f)
 
-
 print("done")
-'''
 
 '''
 Database Structure:
@@ -87,4 +76,12 @@ dark-hair
 long-hair
 brown-eyes
 joy
+'''
+'''
+#RESET DATABASE
+
+faceDatabase = {"by_attr": {"male":{}, "female":{}}, "all":{}}
+
+with open('faceDatabase.pkl', 'wb') as f:  # SAVES DATABASE TO DISK
+    pickle.dump(faceDatabase, f)
 '''
